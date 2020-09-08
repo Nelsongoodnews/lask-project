fa2d2427ec6ad74d4798967a6e2b9ade285446a6
nelsongoodnews <lebabarigoodnews@gmail.com>
2020-09-08 16:36:50 +0100

    fifth commit

--- JAVASCRIPT/main.js
+++ JAVASCRIPT/main.js
@@ -23,3 +23,14 @@ function maindrop(){
     ac.className = "maindrop"
   }
 }
+// $(document).ready(function() {
+//   $(".testimonial-carousel").slick({
+//     infinite: true,
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: false,
+//     arrows: true,
+//     prevArrow: $(".testimonial-carousel-controls .prev"),
+//     nextArrow: $(".testimonial-carousel-controls .next")
+//   });
+// });
